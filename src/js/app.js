9651109f15c10f0fc2cda6a7973345f403f6ca68
Sebas <sebialfano@gmail.com>
2025-03-31 17:15:29 -0300

    Codigo Javascript que incluye las imagenes de la galería

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    crearGaleria()
+})
+
+function crearGaleria(){
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector(".galeria-imagenes")
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
+        const imagen = document.createElement("IMG")
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = "Imagen Galería"
+
+        galeria.appendChild(imagen);
+    }
+}
